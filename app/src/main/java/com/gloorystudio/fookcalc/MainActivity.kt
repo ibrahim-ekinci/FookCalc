10c5d7b21f304538d167da43da68d4b8213cd1d5
ibrahim-ekinci <halil.ibrahim.ekinci@gmail.com>
2021-10-14 14:25:34 +0300

    Added library to test application.

--- app/src/main/java/com/gloorystudio/fookcalc/MainActivity.kt
+++ app/src/main/java/com/gloorystudio/fookcalc/MainActivity.kt
@@ -2,11 +2,15 @@ package com.gloorystudio.fookcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gloorystudio.fook.calc.Fook
+import com.gloorystudio.fook.calc.Fook.Companion.calculate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        println(Fook.calc("44+4"))
+        println("55-2".calculate())
     }
 }
